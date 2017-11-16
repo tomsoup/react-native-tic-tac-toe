3f11bdcdf50dc21b7a56ec6b615c795e7a200265
Tom Ka Chun Cheung <tomhk1123@gmail.com>
2017-11-16 12:37:40 -0500

    implentment restart button

--- App.js
+++ App.js
@@ -1,12 +1,94 @@
 import React from 'react';
-import { StyleSheet, Image, Text, View, Linking, TouchableWithoutFeedback  } from 'react-native';
+import { StyleSheet, Image, Text, View, Linking, TouchableHighlight, TouchableWithoutFeedback  } from 'react-native';
 import Board from './components/Board';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        cells: Array(9).fill(null),
+      }],
+      //playerOne will be X
+      playerOne: true,
+      stepNumber: 0,
+    };
+  }
+
+  calculateWinner = (cells) => {
+    const conditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < conditions.length; i++) {
+      const [a, b, c] = conditions[i];
+      if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+        return cells[a];
+      }
+    }
+    return null;
+  }
+
+  addMark = (position) => {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    //slice to avoid state mutation
+    const cells = current.cells.slice();
+    if (this.calculateWinner(cells) || cells[position]) {
+      return;
+    }
+    // short hand to determine x or o
+    cells[position] = this.state.playerOne ? 'X' : 'O';
+    this.setState({history: history.concat([{
+      cells
+    }]), playerOne: !this.state.playerOne, stepNumber: history.length });
+  }
+
+  restart = (step) => {
+    this.setState({
+      stepNumber: step,
+      playerOne: (step % 2) === 0,
+    });
+  }
+
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = this.calculateWinner(current.cells);
+
+
+    let status;
+      if (winner) {
+          console.log(`${winner} Wins!`);
+      } else {
+          console.log('keep playing');
+      }
+
     return (
       <View style={styles.container}>
-        <Board />
+        <TouchableHighlight
+          activeOpacity={1}
+          style={{
+            backgroundColor: 'green'
+          }}
+          underlayColor='transparent'
+          onPress={
+            () => this.restart(0)
+          }
+        >
+          <Text>Reset </Text>
+        </TouchableHighlight>
+        <Board cells = {current.cells}
+          onCellPress= {(position) => {this.addMark(position)}}
+        />
         <TouchableWithoutFeedback
           onPress={
             () => Linking.openURL('http://tomsoup.io')
--- components/Board.js
+++ components/Board.js
@@ -6,64 +6,15 @@ import { BOARD, BOARD_BORDER } from '../data/Size';
 
 
 export default class Board extends Component {
-  constructor() {
-    super();
-    this.state = {
-      cells: Array(9).fill(null),
-      playerOne: true,
-    };
-  }
-
-
-  addMark = (position) => {
-    //slice to avoid state mutation
-    const cells = this.state.cells.slice();
-    if (this.calculateWinner(cells) || cells[position]) {
-      return;
-    }
-    // short hand to determine x or o
-    cells[position] = this.state.playerOne ? 'X' : 'O';
-    this.setState({cells, playerOne: !this.state.playerOne });
-  }
 
   renderCell = () => {
 
-    return this.state.cells.map((cell, index) =>
-       <Cell cell={cell} key={index} position={index} onCellPress={() => this.addMark(index)}/>
+    return this.props.cells.map((cell, position) =>
+       <Cell cell={cell} key={position} position={position} onCellPress={() => this.props.onCellPress(position)}/>
     );
   }
 
-  calculateWinner = (cells) => {
-    const conditions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < conditions.length; i++) {
-      const [a, b, c] = conditions[i];
-      if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
-        return cells[a];
-      }
-    }
-    return null;
-  }
-
-
   render() {
-
-    const winner = this.calculateWinner(this.state.cells);
-
-    let status;
-      if (winner) {
-          console.log(`${winner} Wins!`);
-      } else {
-          console.log('keep playing');
-      }
 
     return (
       <View style={{
@@ -75,6 +26,7 @@ export default class Board extends Component {
         flexWrap: 'wrap',
         justifyContent: 'space-between'
       }}>
+
         {this.renderCell()}
       </View>
     );
